4004e90136582722218e4e754860597046f7c9f0
Rohitniet <rohitseth252004@gmail.com>
2024-06-09 19:58:30 +0530

    finalizing

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var wind = document.querySelector("#wind")
 var temp = document.querySelector("#temp")
 var disription= document.querySelector("#dis")
 
+function converter(val){
+    return val-273
+}
+
 btn.addEventListener("click",()=>{
     let p =fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityinput.value+'&appid=c134113f4f7f42238837f07533225bbd')
 p.then((value)=>{
@@ -12,7 +16,7 @@ p.then((value)=>{
 }).then((value)=>{
     cityoutput.innerHTML=value.name
     wind.innerHTML=value.wind.speed
-    temp.innerHTML=value.main.temp
+    temp.innerHTML=converter(value.main.temp).toFixed(2)+" degree"
     disription.innerHTML=value.weather[0].description
 })
 })
